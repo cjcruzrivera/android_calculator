1e51e768e60efc141abeb1980571e02e6f771396
melissamillares <diana.millares@correounivalle.edu.co>
2019-11-21 22:35:15 -0500

    funcionamiento de boton 1 y cambios en interfaz

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.calculadora;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    float res = 0;
+    String valor1;
+    String valor2;
+    CharSequence text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        final TextView oper =(TextView)findViewById(R.id.operacion);
+        String operation = oper.getText().toString();
+
+        char v1 = operation.charAt(0);
+        char v2 = operation.charAt(2);
+
+        valor1 = String.valueOf(v1);
+        valor2 = String.valueOf(v2); */
+    }
+
+    public void onClickNum(View view)
+    {
+        final TextView op =(TextView)findViewById(R.id.operacion);
+        final Button btn0 =(Button) findViewById(R.id.btn0);
+        final Button btn1 =(Button) findViewById(R.id.btn1);
+
+        text = btn0.getText();
+        text = btn1.getText();
+        op.append(text);
+        //op.setText(text);
+        text = "";
+    }
+
+    public void onClickIgual(View view)
+    {
+        final TextView result =(TextView)findViewById(R.id.resultado);
+        //float r = sum();
+        //result.setText(String.valueOf(r));
+        result.append(text);
+    }
+
+    private float sum()
+    {
+        float s1 = Float.parseFloat(valor1);
+        float s2 = Float.parseFloat(valor2);
+
+        res = s1 + s2;
+
+        return res;
     }
 }
